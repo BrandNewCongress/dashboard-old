226c359050f09c869a6f27642264cbdf9abe242e
Evan Owski <eowski@gmail.com>
2016-06-05 01:07:45 -0700

    Fix bug where metric internal name was hardcoded to Facebook

--- lib/analytics_integration/metrics_storer.rb
+++ lib/analytics_integration/metrics_storer.rb
@@ -2,7 +2,7 @@ module AnalyticsIntegration
   class MetricsStorer
     class << self
       def store(internal_name, value)
-        metric = Metric.find_by_internal_name("facebook_likes")
+        metric = Metric.find_by_internal_name(internal_name)
         datetime = Util::DateUtil.utcized_eastern_time_date
         mp = MetricPoint.where(datetime: datetime, metric: metric).first_or_initialize
         mp.value = value
