dc7e560039f842d87a21511d3d35bea2032ee5a5
Evan Owski <eowski@gmail.com>
2016-06-04 20:20:37 -0700

    Use Facebook app token instead of user token

--- lib/analytics_integration/facebook.rb
+++ lib/analytics_integration/facebook.rb
@@ -7,10 +7,13 @@ module AnalyticsIntegration
       end
     private
       def fetch_like_count
-        fb_token = ENV["FACEBOOK_OAUTH_TOKEN"]
+        oauth = Koala::Facebook::OAuth.new(ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_APP_SECRET"])
+        fb_token = oauth.get_app_access_token
         @graph = Koala::Facebook::API.new(fb_token)
         page = @graph.get_object(ENV["FACEBOOK_PAGE_ID"], fields: ["fan_count"])
-        page["fan_count"]
+        count = page["fan_count"]
+        puts "Facebook Page Like Count: #{count}"
+        count
       end
     end
   end
